887a80888cce10880307b0ac1639e3001f38be77
Caio Barbosa <caio.barbosa@outlook.com>
2022-03-15 07:52:43 -0300

    first commit

---
+++ lib/ad_helper.dart
@@ -0,0 +1,12 @@
+// ID tests
+// https://developers.google.com/admob/android/test-ads#sample_ad_units
+
+class AdHelper {
+  static String get interstitialAdUnitId {
+    return "ca-app-pub-3940256099942544/1033173712";
+  }
+
+  static String get bannerAdUnitId {
+    return "ca-app-pub-3940256099942544/6300978111";
+  }
+}
